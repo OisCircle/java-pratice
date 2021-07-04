67de77eace971f4d5aea3de0069500023f90cdea
qiuchengquan <qiuchengquan@youzan.com>
2021-07-04 13:52:59 +0800

    xxxx

---
+++ src/algorithm/leetcode/linklist/cycle/HasCycle.java
@@ -0,0 +1,23 @@
+package algorithm.leetcode.linklist.cycle;
+
+/**
+ * @author qiuchengquan
+ * @since 2021/7/4
+ */
+public class HasCycle {
+
+	public boolean hasCycle(ListNode head) {
+		//1.一快一慢判断是否有环
+		ListNode fast = head;
+		ListNode slow = head;
+		//fast跑的快，所以只需要判断fast的next指针
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if (slow == fast) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
---
+++ src/algorithm/leetcode/linklist/cycle/ListNode.java
@@ -0,0 +1,16 @@
+package algorithm.leetcode.linklist.cycle;
+
+/**
+ * @author qiuchengquan
+ * @since 2021/7/4
+ */
+public class ListNode {
+
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+		next = null;
+	}
+}
